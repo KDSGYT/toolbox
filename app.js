68518dcfcc4c0441290fde166b9049f7c3651d5a
KDSGYT <kdsgyt@gmail.com>
2020-06-15 07:29:00 -0400

    Added the list of packages to install

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const { prompt } = require('enquirer');
+const { exec } = require('child_process')
 const checks = require('./lib/check');
+const { emitWarning } = require('process');
+const pkgs = [
+    "vscode",
+    "nodejs"
+];
+//tracking packaged
+let pkgInstalled = 0;
+let pkgNotInstalled = 0;
 
 const root = checks.rootAccess();//check root access
 
 try{
     if(!root) throw Error('Please run the command as sudo');
+
+    //execs the commands
+    console.log('executing the commands');
+    //going over all the packages to install
+    pkgs.forEach( pkg => {
+        let command = `snap install ${pkg} `
+        exec(command, (error) => {
+            try{
+
+            } catch (error){
+                emitWarning( error.message);
+            }
+            console.log(`Package installed: ${pkg}` );
+            pkgInstalled++;
+            
+        });
+    })
+    console.log(`Packages installed: ${pkgInstalled}`)
 
 } catch (error){
     console.log(error.message);
